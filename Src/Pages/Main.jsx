aab222174a4b6e47700cbb8d5b23a93f2fc833f5
Victor Manuel <vijobero139@gmail.com>
2025-08-09 13:43:10 -0500

    Update Main.jsx

--- Src/Pages/Main.jsx
+++ Src/Pages/Main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import './index.css'; // si tienes estilos globales
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
