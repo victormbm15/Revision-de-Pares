06956a34f888f5cacdb3124f4c96f642be9c10a0
Victor Manuel <vijobero139@gmail.com>
2025-08-09 13:54:32 -0500

    Update Main.jsx

--- Src/Pages/Main.jsx
+++ Src/Pages/Main.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // si tienes estilos globales
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
+import { restoreCart } from './Redux/CartSlice';
+
+// Restaurar carrito desde localStorage
+const savedCart = localStorage.getItem('cart');
+if (savedCart) {
+  try {
+    const parsedCart = JSON.parse(savedCart);
+    store.dispatch(restoreCart(parsedCart));
+  } catch (error) {
+    console.error('Error al restaurar el carrito:', error);
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
